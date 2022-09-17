ae8627475d67d0fac9109036f2820d9883f62f2a
twogod23 <60266261.futagami.cgp@gmail.com>
2022-09-18 00:48:49 +0900

    202209180048

--- team2021-01/Assets/Scripts/OpenPauseMenu.cs
+++ team2021-01/Assets/Scripts/OpenPauseMenu.cs
@@ -1,26 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class OpenPauseMenu : MonoBehaviour
 {
+    //ポーズメニュー画面の指定
     public GameObject pauseMenu;
+    //ポーズメニュー画面へ遷移するボタンの指定
+    public GameObject pauseButton;
 
     void Start()
     {
         pauseMenu.SetActive(false);
+        pauseButton.SetActive(true);
     }
     
     //ポーズメニューを有効にする
     public void open()
     {
         pauseMenu.SetActive(true);
-    }
-
-    //ポーズメニューを無効にする
-    public void close()
-    {
-        pauseMenu.SetActive(false);
+        pauseButton.SetActive(false);
     }
 }
