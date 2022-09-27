3d0b99b7c772b1e372d6d4bef2da46a2d717dae6
twogod23 <60266261.futagami.cgp@gmail.com>
2022-09-27 15:07:12 +0900

    202209271507

--- team2021_22summer/Assets/Scripts/ItemMove.cs
+++ team2021_22summer/Assets/Scripts/ItemMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoodleMove : MonoBehaviour
+public class ItemMove : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
