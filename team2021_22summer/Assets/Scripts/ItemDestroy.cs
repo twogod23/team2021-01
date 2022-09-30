c9bf4b7331e7321d2aee8796aaa7836e43ac01d1
twogod23 <60266261.futagami.cgp@gmail.com>
2022-09-30 23:02:22 +0900

    202209302302

--- team2021_22summer/Assets/Scripts/ItemDestroy.cs
+++ team2021_22summer/Assets/Scripts/ItemDestroy.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SomenDestroy : MonoBehaviour
+public class ItemDestroy : MonoBehaviour
 {
-    public GameObject somen;
+    public GameObject item;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +14,11 @@ public class SomenDestroy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float posN = somen.transform.position.x;
+        float posN = item.transform.position.x;
 
         if (posN > 35.0f)
         {
-            Destroy(somen);
+            Destroy(item);
         }
     }
 }
